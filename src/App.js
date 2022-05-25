b1d13fc6fea15980f0711784b12f1ba42cfe30a1
Jeff Andrews <88903188+cognizantJeff@users.noreply.github.com>
2022-05-25 08:51:14 -0400

    course finished, pushing to cloud for reference

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState, useRef } from "react";
 
-function App() {
+export default function App() {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("react hooks");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const searchInputRef = useRef();
+
+  useEffect(() => {
+    getResults();
+    // .then((response) => {
+    //   console.log(response.data);
+    //   setResults(response.data.hits);
+    // });
+  }, []); //use dependency array to only call on load - things you want to load on changes get entered to dep array
+
+  const getResults = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      console.log(response.data);
+      setResults(response.data.hits);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getResults();
+  };
+
+  const handleClear = () => {
+    setQuery("");
+    searchInputRef.current.focus();
+  };
+  //in class based components, refs give us a way to access elements created in the render method.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="container max-w-md 
+    mx-auto p-4 m-2 bg-purple-lightest 
+    shadow-lg rounded"
+    >
+      <img
+        src="https://icon.now.sh/react/c0c"
+        alt="react-logo"
+        className="float-right h-12"
+      />
+      <h1 className="text-grey-darkest font-thin">Hooks News</h1>
+      <form onSubmit={handleSubmit} className="mb-2">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          ref={searchInputRef}
+          className="border p-1 rounded"
+        />
+        <button type="submit" className="bg-orange rounded m-1 p-1">
+          get news!
+        </button>
+        <button
+          type="button"
+          onClick={handleClear}
+          className="bg-teal text-white p-1 rounded"
         >
-          Learn React
-        </a>
-      </header>
+          Clear
+        </button>
+      </form>
+      {loading ? (
+        <div className="font-bold text-orange-dark">"loading..."</div>
+      ) : (
+        <div>
+          <ul className="list-reset leading-normal">
+            {results.map((result) => (
+              <li key={result.objectID}>
+                <a
+                  className="text-indigo-dark hover:text-indigo-darkest"
+                  href={result.url}
+                >
+                  {result.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {error && <div className="text-red font-bold">{error.message}</div>}
     </div>
   );
 }
-
-export default App;
